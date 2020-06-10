f8b528b75f6613789072616b492f8fb3e0e5847a
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-06-10 11:32:38 +0200

    Create Power.h

---
+++ Data structure/Assignment-1/Problem 4/Power.h
@@ -0,0 +1,11 @@
+#ifndef POWER_H_INCLUDED
+#define POWER_H_INCLUDED
+
+
+long long Power1(int a,unsigned int n);
+long long Power2(int a,unsigned int n);
+void menu();
+
+
+
+#endif // POWER_H_INCLUDED
