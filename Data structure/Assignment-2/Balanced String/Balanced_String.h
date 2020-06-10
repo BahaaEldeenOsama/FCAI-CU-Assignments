90d6bec30f9069890665d8b9c8c19b629810911b
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-06-10 11:49:24 +0200

    Create Balanced_String.h

---
+++ Data structure/Assignment-2/Balanced String/Balanced_String.h
@@ -0,0 +1,14 @@
+#ifndef BALANCED_STRING_H_INCLUDED
+#define BALANCED_STRING_H_INCLUDED
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep0(i,sz)   for(int i=0;i<sz;++i)
+bool check(char open ,char close);
+bool S2P2(string exp);
+
+#endif // BALANCED_STRING_H_INCLUDED
+
+
+
+
