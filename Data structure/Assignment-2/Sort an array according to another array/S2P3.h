f66fc4ed92e565325db16a75029e99645613bb05
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-06-10 11:55:56 +0200

    Create S2P3.h

---
+++ Data structure/Assignment-2/Sort an array according to another array/S2P3.h
@@ -0,0 +1,13 @@
+#ifndef S2P3_H_INCLUDED
+#define S2P3_H_INCLUDED
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep0(i,n)   for(int i=0;i<n;++i)
+void S2P3();
+
+
+#endif // S2P3_H_INCLUDED
+
+
+
