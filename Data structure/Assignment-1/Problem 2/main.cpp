971ec3510292226ac26384c8d6918c7bcd733125
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-06-10 11:26:32 +0200

    Create main.cpp

---
+++ Data structure/Assignment-1/Problem 2/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include"Fraction.h"
+using namespace std;
+
+
+
+
+
+int main()
+{
+  cout << "\t\t\t\t\t\t\t Welcome to FCI Fraction Calculator \n\n\n\n\n\n\n\n\n";
+   menu();
+
+return 0;}
