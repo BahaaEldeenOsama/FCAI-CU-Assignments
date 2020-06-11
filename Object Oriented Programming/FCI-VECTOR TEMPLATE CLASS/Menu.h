4402df68688613204836b984f21f267c08d583df
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-06-11 03:14:45 +0200

    Create Menu.h

---
+++ Object Oriented Programming/FCI-VECTOR TEMPLATE CLASS/Menu.h
@@ -0,0 +1,23 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include<bits/stdc++.h>
+#include "FCI-VECTOR.h"
+#include "FCI_COURS.h"
+#include "FCI_STUDENT.h"
+using namespace std ;
+
+
+
+void manu();
+char my_Choice(int i);
+
+void list_of_integer();
+void list_of_character();
+void list_of_string();
+void list_of_float();
+void list_of_students();
+
+
+
+
+#endif // MENU_H_INCLUDED
