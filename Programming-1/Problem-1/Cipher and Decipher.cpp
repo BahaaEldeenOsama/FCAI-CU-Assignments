801d4beebea3ee91c366825ecef2fe8bb798544c
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-06-11 02:06:29 +0200

    Update and rename Cipher.cpp to Cipher and Decipher.cpp

---
+++ Programming-1/Problem-1/Cipher and Decipher.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void encryptDecrypt(char inpString[], char key )
+{
+    // Define XOR key
+    // Any character value will work
+    char xorKey = key;
+
+    // calculate length of input string
+    int len = strlen(inpString);
+
+    // perform XOR operation of key
+    // with every character in string
+    for (int i = 0; i < len; i++)
+    {
+        inpString[i] = inpString[i] ^ xorKey;
+        printf("%c",inpString[i]);
+    }
+}
+
+
+int main(void)
+{
+int x =0 ;
+
+
+
+
+cout<<"Ahlan ya user ya habibi." <<endl ;
+cout<<"If you want to Cipher a message Enter number 1: " <<endl;
+cout<<"If you want to Decipher a message Enter number 2: " <<endl;
+cout<<"If you want to End Enter number 3: " <<endl ;
+cin>> x ;
+
+
+
+if(x==1)
+{
+
+    char sampleString[100];
+
+    char key;
+
+    cout<<"Please enter your massage : ";
+
+    cin>>sampleString;
+
+    cout<<"Please enter your key : ";
+
+    cin>>key;
+
+
+ // Encrypt the string
+    printf("Encrypted String: ");
+    encryptDecrypt(sampleString,key);
+    printf("\n");
+
+}
+
+else if (x==2)
+{
+
+
+ char sampleString[100];
+
+    char key;
+
+    cout<<"Please enter your massage : ";
+
+    cin>>sampleString;
+
+    cout<<"Please enter your key : ";
+
+    cin>>key;
+
+    // Decrypt the string
+    printf("Decrypted String: ");
+    encryptDecrypt(sampleString,key);
+}
+
+
+
+else if (x==3)
+{
+          cout<<"Exit " <<endl ;
+}
+
+return 0 ;
+}
